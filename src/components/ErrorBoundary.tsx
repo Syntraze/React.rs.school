f1abb0c0538589af99b50b21a70f7994facbb6d5
Syntraze <syntraze@gmail.com>
2025-07-13 14:54:47 +0200

    feat:add error boundary

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<React.PropsWithChildren, State> {
+  constructor(props: React.PropsWithChildren) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    console.error("Caught by Error Boundary:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className="text-red-600">
+          <h2>Something went wrong.</h2>
+          <button onClick={() => window.location.reload()}>Reload Page</button>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
